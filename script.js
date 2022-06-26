424af1b32f3f8b96aaa1234aee95f4b15d727b7e
Victoria <tory1312@list.ru>
2022-06-26 20:48:50 +0300

    first hw

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-let number = 1;
-const leftBorderWidth = 5;
-number =10;
-console.log(number);
+'use strict'
+
+let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+let lastFilm = prompt('Один из последних просмотренных фильмов?', '');
+let filmScore = prompt('На сколько оцените его?', '');
+let lastFilm2 = prompt('Один из последних просмотренных фильмов?', '');
+let filmScore2 = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[lastFilm] = filmScore;
+personalMovieDB.movies[lastFilm2] = filmScore2;
+
+console.log(personalMovieDB);
